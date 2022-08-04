b495b309c81de15877804db04e8f0d67e4b20019
seungmin lim <seungmin3238@gmail.com>
2022-08-04 13:06:47 +0900

    ADD :공동 Nav 초기 작업 완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import './Nav.scss';
 
 const Nav = () => {
   return (
-    <div>
-      Nav
-    </div>
+    <nav>
+      <div className="mainLogo">
+        <i className="fa-brands fa-instagram" />
+        <div className="logoLine" />
+        <h1>Westagram</h1>
+      </div>
+
+      <input className="search" type="text" placeholder="검색" />
+
+      <div className="mainMenu">
+        <img
+          className="menuImg explore"
+          src="/images/explore.png"
+          alt="explore"
+        />
+        <img className="menuImg herat" src="/images/heart.png" alt="heart" />
+        <img
+          className="menuImg profile"
+          src="/images/profile.png"
+          alt="profile"
+        />
+      </div>
+    </nav>
   );
-}
+};
 
 export default Nav;
