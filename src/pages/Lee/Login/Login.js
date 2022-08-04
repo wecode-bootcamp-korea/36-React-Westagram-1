602d644d543ab66c10b961bc34ff5d17ed29e24f
CodyMan0 <hys83751952@gmail.com>
2022-08-04 21:50:22 +0900

    Implementing user input data storage (revision 2)

--- src/pages/Lee/Login/Login.js
+++ src/pages/Lee/Login/Login.js
@@ -26,7 +26,7 @@ const Login = () => {
         <div className="sub_container">
           <input
             className="inputClass"
-            id="id"
+            value={id}
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
             onChange={saveUserId}
