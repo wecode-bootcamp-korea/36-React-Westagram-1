3e0067da9c5c63702a3321de33f6e80fb8ec768a
seungmin lim <seungmin3238@gmail.com>
2022-08-06 17:11:50 +0900

    [36기 임승민] Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/Lim/Login/Login.js
+++ src/pages/Lim/Login/Login.js
@@ -9,38 +9,45 @@ function Login() {
   const saveUserId = event => {
     setId(event.target.value);
   };
+
   const [pw, setPw] = useState('');
   const saveUserPw = event => {
     setPw(event.target.value);
   };
-  return (
-    <>
-      <div className="loginBox">
-        <h1 className="loginLogo">Westagram</h1>
-        <form className="loginForm">
-          <input
-            className="id input"
-            type="text"
-            placeholder="전화번호, 사용자 이름 또는 이메일"
-            onChange={saveUserId}
-            value={id}
-          />
 
-          <input
-            className="pw input"
-            type="password"
-            placeholder="비밀번호"
-            onChange={saveUserPw}
-            value={pw}
-          />
-          <button className="loginBtn" onClick={() => navigate('/main')}>
-            로그인
-          </button>
-        </form>
-        <a href="">비밀번호를 잊으셨나요?</a>
-      </div>
-      <script defer src="js/login.js" />
-    </>
+  const validation = (id, pw) => {
+    return id.indexOf('@') !== -1 && pw.length >= 5 ? false : true;
+  };
+
+  return (
+    <div className="loginBox">
+      <h1 className="loginLogo">Westagram</h1>
+      <form className="loginForm">
+        <input
+          className="id input"
+          type="text"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+          onChange={saveUserId}
+          value={id}
+        />
+
+        <input
+          className="pw input"
+          type="password"
+          placeholder="비밀번호"
+          onChange={saveUserPw}
+          value={pw}
+        />
+        <button
+          className="loginBtn"
+          onClick={() => navigate('/main')}
+          disabled={validation(id, pw)}
+        >
+          로그인
+        </button>
+      </form>
+      <a href="null">비밀번호를 잊으셨나요?</a>
+    </div>
   );
 }
 
