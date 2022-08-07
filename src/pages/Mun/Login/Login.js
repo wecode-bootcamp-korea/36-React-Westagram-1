595cac43d0d3bc8dfe9f873d08d9082ad567a075
junki <junki646@gmail.com>
2022-08-08 01:34:12 +0900

    Add: js에서 리액트로 기본 변환 완료

--- src/pages/Mun/Login/Login.js
+++ src/pages/Mun/Login/Login.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import './Login.scss'
 
-const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  );
+function Login() {
+    return (
+            <div className="loginContainer">
+                <h1 className="loginTitle">
+                    westagram
+                </h1>
+                <div className="loginBox">
+                    <input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" id="id" />
+                    <input type="password" placeholder="비밀번호" id="password" />
+                </div>
+                <button id="loginButton">
+                            로그인
+                </button>
+                <div id="error">
+                </div>
+                <a href="" className="question">비밀번호를 잊으셨나요?</a>
+            </div>
+    );
 }
 
 export default Login;
