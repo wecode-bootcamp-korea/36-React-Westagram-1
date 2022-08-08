362c958e65a093fbf66a136ad8fb3f0016ff4975
junki <junki646@gmail.com>
2022-08-08 13:21:23 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/Mun/Login/Login.js
+++ src/pages/Mun/Login/Login.js
@@ -1,8 +1,20 @@
 import './Login.scss'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 function Login() {
+    const [id,setId] = useState(null)
+    const [password,setPassword] = useState(null)
+
     const navigate = useNavigate();
+
+    function saveUserId(e){
+        setId(e.target.value);
+    }
+
+    function saveUserPassword(e){
+        setPassword(e.target.value);
+    }
 
     return (
             <div className="loginContainer">
@@ -10,8 +22,8 @@ function Login() {
                     westagram
                 </h1>
                 <div className="loginBox">
-                    <input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" id="id" />
-                    <input type="password" placeholder="비밀번호" id="password" />
+                    <input onChange={(e)=>{saveUserId(e)}} type="text" placeholder="전화번호, 사용자 이름 또는 이메일" id="id" />
+                    <input onChange={(e)=>{saveUserPassword(e)}} type="password" placeholder="비밀번호" id="password" />
                 </div>
                 <button id="loginButton" onClick={()=>{navigate('/main')}}>
                             로그인
