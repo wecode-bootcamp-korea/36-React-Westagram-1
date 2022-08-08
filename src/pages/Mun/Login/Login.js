1586b79c5039e69766da50b413e6d755db5a44db
junki <junki646@gmail.com>
2022-08-08 13:37:04 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/Mun/Login/Login.js
+++ src/pages/Mun/Login/Login.js
@@ -1,38 +1,58 @@
-import './Login.scss'
-import { useNavigate } from 'react-router-dom'
-import { useState } from 'react'
+import './Login.scss';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function Login() {
-    const [id,setId] = useState(null)
-    const [password,setPassword] = useState(null)
+  const navigate = useNavigate();
+  const [id, setId] = useState(null);
+  const [password, setPassword] = useState(null);
+  let btnDisabled = true;
 
-    const navigate = useNavigate();
+  function saveUserId(e) {
+    e.target.id === 'id' ? setId(e.target.value) : setPassword(e.target.value);
+  }
 
-    function saveUserId(e){
-        setId(e.target.value);
+  function validation() {
+    if (id?.includes('@') && password?.length > 4) {
+      btnDisabled = false;
+      return 'loginButtonActive';
+    } else {
+      btnDisabled = true;
+      return 'loginButton';
     }
+  }
 
-    function saveUserPassword(e){
-        setPassword(e.target.value);
-    }
-
-    return (
-            <div className="loginContainer">
-                <h1 className="loginTitle">
-                    westagram
-                </h1>
-                <div className="loginBox">
-                    <input onChange={(e)=>{saveUserId(e)}} type="text" placeholder="전화번호, 사용자 이름 또는 이메일" id="id" />
-                    <input onChange={(e)=>{saveUserPassword(e)}} type="password" placeholder="비밀번호" id="password" />
-                </div>
-                <button id="loginButton" onClick={()=>{navigate('/main')}}>
-                            로그인
-                </button>
-                <div id="error">
-                </div>
-                <a href="" className="question">비밀번호를 잊으셨나요?</a>
-            </div>
-    );
+  return (
+    <div className="loginContainer">
+      <h1 className="loginTitle">westagram</h1>
+      <div className="loginBox">
+        <input
+          onChange={e => {
+            saveUserId(e);
+          }}
+          type="text"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+          id="id"
+        />
+        <input
+          onChange={e => {
+            saveUserId(e);
+          }}
+          type="password"
+          placeholder="비밀번호"
+          id="password"
+        />
+      </div>
+      <button
+        className={validation()}
+        onClick={() => {
+          navigate('/main');
+        }}
+        disabled={btnDisabled}
+      >
+        로그인
+      </button>
+    </div>
+  );
 }
-
 export default Login;
