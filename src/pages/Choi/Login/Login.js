ceefcc147c4da83c06327b92233a94851017faf6
Qrio <kyuchoi@YUNMIui-MacBookAir.local>
2022-08-08 17:54:51 +0900

    Add: Mission2 : 로그인 버튼 활성화 완료

--- src/pages/Choi/Login/Login.js
+++ src/pages/Choi/Login/Login.js
@@ -2,16 +2,21 @@ import React, { useState } from 'react';
 import './Login.scss';
 
 const Login = () => {
-  const [userIdSaved, setUserIdSaved] = useState(null);
-  const [userPwSaved, setUserPwSaved] = useState(null);
-
-  const saveUserId = event => {
-    return setUserIdSaved((userIdSaved = event.target.value));
+  let [userIdSaved, setUserIdSaved] = useState('');
+  let [userPwSaved, setUserPwSaved] = useState('');
+  const userIdSet = event => {
+    return setUserIdSaved(event.target.value);
   };
 
-  const saveUserPw = event => {
-    return setUserPwSaved((userPwSaved = event.target.value));
+  const userPwSet = event => {
+    console.log(event.target.value);
+    setUserPwSaved(event.target.value);
   };
+
+  console.log(userPwSaved);
+
+  const inputValid = userIdSaved.includes('@') && userPwSaved.length > 5;
+  console.log(inputValid);
 
   return (
     <body className="loginBody">
@@ -22,16 +27,25 @@ const Login = () => {
             className="userName"
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
-            onChange={saveUserId}
+            onChange={userIdSet}
           />
           <input
             className="passwordInput"
             type="password"
             placeholder="비밀번호"
-            onChage={saveUserPw}
+            onChange={userPwSet}
           />
 
-          <button>로그인</button>
+          <button
+            style={
+              inputValid
+                ? { backgroundColor: 'blue' }
+                : { backgroundColor: '#acd0e6f' }
+            }
+            disabled={inputValid ? false : true}
+          >
+            로그인
+          </button>
         </div>
         <div className="forgotPassword">비밀번호를 잊으셨나요?</div>
       </div>
