7d51a068a1fb5a5e4b080643c98d08b08e1a98e6
CodyMan0 <hys83751952@gmail.com>
2022-08-09 12:50:30 +0900

    refactoring process - 상수 데이터를 활용하여 중복되는 코드 및 데이터 정리

--- src/pages/Lee/Main/Main.js
+++ src/pages/Lee/Main/Main.js
@@ -19,10 +19,7 @@ const Main = () => {
             <div className="new-feed middleText">새 게시물</div>
           </div>
           <div className="feedContainer">
-            <Feed />
-            <Feed />
-            <Feed />
-            <Feed />
+            <Feed data={USER_INFORMATIONS} feed={FEED_INFORMATION} />
           </div>
           <div className="main-right">
             <div className="profile">
@@ -44,7 +41,7 @@ const Main = () => {
               <div className="middleText">모두 보기</div>
             </div>
             <div className="story">
-              {PROFILE_INFORMATIONS.map(item => (
+              {USER_INFORMATIONS.map(item => (
                 <div key={item.id} className="story__profile">
                   <img
                     className="img__round-small"
@@ -82,41 +79,128 @@ const FEED_STORY_IMGS = [
   { id: 6, src: 'images/Lee/ANGMOND.jpeg', alt: 'argmond', name: 'argmond' },
 ];
 
-const PROFILE_INFORMATIONS = [
+const USER_INFORMATIONS = [
   {
     id: 1,
     src: 'images/Lee/프3.jpeg',
     alt: 'profilePicture',
     name: 'dev_codyman0',
+    subName: '오늘은 여기까지1',
     feature: 'deanthony_hatter님이 팔로우합니다.',
+    articleSrc: 'images/Lee/피드.jpg',
+    articleAlt: 'big-picture',
+    reply: '댓글 기능을 구현해보자!',
+    heart: '2312414명',
   },
   {
     id: 2,
     src: 'images/Lee/프3.jpeg',
     alt: 'profilePicture',
     name: 'wecode__bootcamp',
+    subName: '오늘은 여기까지2',
     feature: 'we_are_code님 외 1명이 팔로우합니다',
+    articleSrc: 'images/Lee/피드.jpg',
+    articleAlt: 'big-picture',
+    saying: '!',
+    heart: '414212명',
   },
   {
     id: 3,
     src: 'images/Lee/프3.jpeg',
     alt: 'profilePicture',
     name: 'that_techsiss',
+    subName: '오늘은 여기까지3',
     feature: 'Instagram 신규 가입',
+    articleSrc: 'images/Lee/피드.jpg',
+    articleAlt: 'big-picture',
+    saying: '댓글 기능을 구현해보자!',
+    heart: '22343명',
   },
   {
     id: 4,
     src: 'images/Lee/프3.jpeg',
     alt: 'profilePicture',
     name: 'edoc_code',
+    subName: '오늘은 여기까지4',
     feature: 'Instagram 신규 가입',
+    articleSrc: 'images/Lee/피드.jpg',
+    articleAlt: 'big-picture',
+    saying: '댓글 기능을 구현해보자!',
+    heart: '10023명',
   },
   {
     id: 5,
     src: 'images/Lee/프3.jpeg',
     alt: 'profilePicture',
     name: 'codingpathsala',
+    subName: '오늘은 여기까지5',
     feature: '에러 deanthony_hatter님 외 1명이 팔로우...',
+    articleSrc: 'images/Lee/피드.jpg',
+    articleAlt: 'big-picture',
+    saying: '댓글 기능을 구현해보자!',
+    heart: '9명',
+  },
+];
+
+const FEED_INFORMATION = [
+  {
+    id: 1,
+    src: 'images/Lee/프3.jpeg',
+    alt: 'profilePicture',
+    name: 'dev_codyman0',
+    subName: '오늘은 여기까지1',
+    feature: 'deanthony_hatter님이 팔로우합니다.',
+    articleSrc: 'images/Lee/피드.jpg',
+    articleAlt: 'big-picture',
+    reply: '피드 게시 글 1',
+    heart: '2312414명',
+  },
+  {
+    id: 2,
+    src: 'images/Lee/프3.jpeg',
+    alt: 'profilePicture',
+    name: 'wecode__bootcamp',
+    subName: '오늘은 여기까지2',
+    feature: 'we_are_code님 외 1명이 팔로우합니다',
+    articleSrc: 'images/Lee/피드.jpg',
+    articleAlt: 'big-picture',
+    saying: '피드 게시 글 2!',
+    heart: '414212명',
+  },
+  {
+    id: 3,
+    src: 'images/Lee/프3.jpeg',
+    alt: 'profilePicture',
+    name: 'that_techsiss',
+    subName: '오늘은 여기까지3',
+    feature: 'Instagram 신규 가입',
+    articleSrc: 'images/Lee/피드.jpg',
+    articleAlt: 'big-picture',
+    saying: '피드 게시 글 3!',
+    heart: '22343명',
+  },
+  {
+    id: 4,
+    src: 'images/Lee/프3.jpeg',
+    alt: 'profilePicture',
+    name: 'edoc_code',
+    subName: '오늘은 여기까지4',
+    feature: 'Instagram 신규 가입',
+    articleSrc: 'images/Lee/피드.jpg',
+    articleAlt: 'big-picture',
+    saying: '피드 게시 글 4!',
+    heart: '10023명',
+  },
+  {
+    id: 5,
+    src: 'images/Lee/프3.jpeg',
+    alt: 'profilePicture',
+    name: 'codingpathsala',
+    subName: '오늘은 여기까지5',
+    articleSrc: 'images/Lee/피드.jpg',
+    articleAlt: 'big-picture',
+    saying: '피드 게시 글 5',
+    heart: '9명',
   },
 ];
 
--- src/pages/Lee/feed/Feed.js
+++ src/pages/Lee/feed/Feed.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Reply from '../reply/Reply';
+import React, { useRef, useState } from 'react';
 import './feed.scss';
 
-const Feed = () => {
+const Feed = props => {
   const [value, setValue] = useState('');
   const [comments, setComments] = useState([]);
   const reply = useRef(null);
@@ -19,38 +18,34 @@ const Feed = () => {
     registerComment(e);
     setValue('');
   };
-
+  // scroll시도
   // const scrollToBottom = () => {
   //   reply.current.scrollIntoView({ behavior: 'smooth' });
   // };
 
-  useEffect(() => {
-    comments.current?.scrollIntoView();
-  }, [comments]);
+  // useEffect(() => {
+  //   comments.current?.scrollIntoView();
+  // }, [comments]);
 
-  return (
-    <div className="feed">
+  return props.feed.map(info => (
+    <div key={info.id} className="feed">
       <div className="feeds">
         <div className="feeds__name story__profile">
-          <img
-            className="img__round-small"
-            src="images/Lee/프3.jpeg"
-            alt="피드"
-          />
+          <img className="img__round-small" src={info.src} alt={info.alt} />
           <div className="profile__text">
             <div className="middleText">
-              <h4>dev_codyman0</h4>
+              <h4>{info.name}</h4>
             </div>
-            <div className="smallText">오늘은 여기까지</div>
+            <div className="smallText">{info.subName}</div>
           </div>
           <i className="uil uil-ellipsis-h icon icon-absolute" />
         </div>
         <article>
           <img
             className="article__img"
-            src="images/Lee/피드.jpg"
+            src={info.articleSrc}
             width="100%"
-            alt="nope"
+            alt={info.articleAlt}
           />
         </article>
         <div className="push__icon">
@@ -61,17 +56,24 @@ const Feed = () => {
         </div>
         <ul className="replSection" ref={reply}>
           <li className="repl middleText">
-            <span className="repl middleText">
-              juyoung님 외 3명이 좋아합니다
+            <span className="repliment middleText">
+              <span className="receivedName">{info.name}</span>님 외
+              <span className="receiveHeart">{info.heart}</span>이 좋아합니다
             </span>
-            <span className="name">dev_codyman0</span>
-            <span className="smallText">댓글 기능을 구현해보자! </span>
+            <span className="name">{info.name}</span>
+            <span className="smallText">{info.saying} </span>
             <button className="smallText" type="submit">
               ...더 보기
             </button>
-            <span className="repl smallText reply">댓글 2개 다시보기</span>
+            <span className="repliment smallText reply">댓글 2개 다시보기</span>
           </li>
-          <Reply comment={comments} />
+
+          {comments.map((comment, idx) => (
+            <li key={idx}>
+              <span className="name">{info.name}</span>
+              <span className="smallText">{comment}</span>
+            </li>
+          ))}
         </ul>
         <form className="feeds__input">
           <i className="uil uil-smile icon" />
@@ -88,7 +90,7 @@ const Feed = () => {
         </form>
       </div>
     </div>
-  );
+  ));
 };
 
 export default Feed;
