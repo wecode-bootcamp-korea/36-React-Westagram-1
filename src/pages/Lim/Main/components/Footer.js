d46c2fe3ed18afc938b958f34e87088d92aba948
seungmin lim <seungmin3238@gmail.com>
2022-08-10 01:43:09 +0900

    Aside에서 상수 데이터 활용

--- src/pages/Lim/Main/components/Footer.js
+++ src/pages/Lim/Main/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FOOTER_INFO } from './footerData';
 import './Footer.scss';
 
 function Footer() {
@@ -17,15 +18,3 @@ function Footer() {
   );
 }
 export default Footer;
-
-const FOOTER_INFO = [
-  { id: 1, content: '소개 ·', url: '/null' },
-  { id: 2, content: '도움말 ·', url: '/null' },
-  { id: 3, content: '홍보센터 ·', url: '/null' },
-  { id: 4, content: 'API ·', url: '/null' },
-  { id: 8, content: '채용 정보 ·', url: '/null' },
-  { id: 5, content: '개인정보처리방침 ·', url: '/null' },
-  { id: 6, content: '약관 ·', url: '/null' },
-  { id: 7, content: '위치 ·', url: '/null' },
-  { id: 10, content: '언어', url: '/null' },
-];
